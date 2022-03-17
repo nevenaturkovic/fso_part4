02252905b06fc2a7877e2f1320726959aa07144d
Nevena Turkovic <nevenatu@gmail.com>
2022-03-17 14:53:49 +0100

    Finished 4.5*: helper functions and unit tests, step3

--- bloglist/utils/list_helper.js
+++ bloglist/utils/list_helper.js
@@ -3,17 +3,28 @@ const dummy = (blogs) => {
 }
 
 const totalLikes = (blogs) => {
-
   const reducer = (sum, item) => {
     return sum + item.likes
   }
 
+  return blogs.length === 0 ? 0 : blogs.reduce(reducer, 0)
+}
+
+const favoriteBlog = (blogs) => {
+  const maxLikes = Math.max(...blogs.map((blog) => blog.likes))
+  const blogWithMaxLikes = blogs.find((blog) => blog.likes === maxLikes)
+
   return blogs.length === 0
     ? 0
-    : blogs.reduce(reducer, 0)
+    : {
+        title: blogWithMaxLikes.title,
+        author: blogWithMaxLikes.author,
+        likes: blogWithMaxLikes.likes,
+      }
 }
 
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 }
