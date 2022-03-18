7790cb95c2519a3de33bf63a916c44443f22fb2b
Nevena Turkovic <nevenatu@gmail.com>
2022-03-18 22:42:56 +0100

    Finished 4.9*: Blog list tests, step2

--- bloglist/tests/blog_api.test.js
+++ bloglist/tests/blog_api.test.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose")
 const supertest = require("supertest")
-const helper = require('./test_helper')
+const helper = require("./test_helper")
 const app = require("../app")
 const api = supertest(app)
 const Blog = require("../models/blog")
@@ -8,13 +8,11 @@ const Blog = require("../models/blog")
 beforeEach(async () => {
   await Blog.deleteMany({})
 
-  let blogObject = new Blog(helper.initialBlogs[0])  
-  await blogObject.save()
-
-  blogObject = new Blog(helper.initialBlogs[1])  
-  await blogObject.save()
+  for (let blog of helper.initialBlogs) {
+    let blogObject = new Blog(blog)
+    await blogObject.save()
+  }
 })
-
 
 test("blogs are returned as json", async () => {
   await api
@@ -27,6 +25,13 @@ test("all blogs are returned", async () => {
   const response = await api.get("/api/blogs")
 
   expect(response.body).toHaveLength(helper.initialBlogs.length)
+})
+
+test("blogs have ids", async () => {
+  const response = await api.get("/api/blogs")
+  for (let blog of response.body) {
+    expect(blog.id).toBeDefined()
+  }
 })
 
 afterAll(() => {
