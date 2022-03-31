38e8a4e2b382065044bf132d34ca232eab0b7a6c
Nevena Turkovic <nevenatu@gmail.com>
2022-03-31 17:24:36 +0200

    Finished 4.6 and 4.7

--- bloglist/utils/list_helper.js
+++ bloglist/utils/list_helper.js
@@ -1,3 +1,5 @@
+var _ = require("lodash")
+
 const dummy = (blogs) => {
   return 1
 }
@@ -23,8 +25,59 @@ const favoriteBlog = (blogs) => {
       }
 }
 
+const mostBlogs = (blogs) => {
+  if (blogs.length !== 0) {
+    const blogsByAuthor = _.countBy(blogs, "author")
+    const maxBlogs = Math.max(...Object.values(blogsByAuthor))
+    const [authorWithMostBlogs, _maxBlogs] = Object.entries(blogsByAuthor).find(
+      ([_author, blogs]) => blogs === maxBlogs
+    )
+
+    return {
+      author: authorWithMostBlogs,
+      blogs: maxBlogs,
+    }
+  }
+  return null
+}
+
+const mostLikes = (blogs) => {
+  if (blogs.length !== 0) {
+    var authorsAndLikes = blogs.reduce((aal, blog) => {
+      if (blog.author in aal) {
+        aal[blog.author] += blog.likes
+      } else {
+        aal[blog.author] = blog.likes
+      }
+      return aal
+    }, {})
+
+    // var authorsAndLikes = {}
+    // for (const blog of blogs) {
+    //   if (blog.author in authorsAndLikes) {
+    //     authorsAndLikes[blog.author] += blog.likes
+    //   } else {
+    //     authorsAndLikes[blog.author] = blog.likes
+    //   }
+    // }
+
+    const maxLikes = Math.max(...Object.values(authorsAndLikes))
+    const [authorWithMostLikes, _maxLikes] = Object.entries(
+      authorsAndLikes
+    ).find(([_author, likes]) => likes === maxLikes)
+
+    return {
+      author: authorWithMostLikes,
+      likes: maxLikes,
+    }
+  }
+  return null
+}
+
 module.exports = {
   dummy,
   totalLikes,
   favoriteBlog,
+  mostBlogs,
+  mostLikes,
 }
